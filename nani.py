cb721bd89c0da6458bb8cc05bfaae57b575c1862
Lex Shornikov <lex.shornikov@yandex.ru>
2021-04-19 19:23:53 +0300

    def equations is created

--- nani.py
+++ nani.py
@@ -1,11 +1,13 @@
 # Импортируем необходимые классы.
 import datetime
+import math
 
 from telegram.ext import CommandHandler
 from telegram.ext import Updater, MessageHandler, Filters
 
 now = datetime.datetime.now()
 TOKEN = '1678923601:AAHs2GdKvsjcixPC9S4C1e9y2C78Z23-V80'
+
 
 # Определяем функцию-обработчик сообщений.
 # У неё два параметра, сам бот и класс updater, принявший сообщение.
@@ -25,7 +27,7 @@ def main():
 
     # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("time", time))
+    dp.add_handler(CommandHandler("equations", equations))
     dp.add_handler(CommandHandler("date", date))
 
     # Создаём обработчик сообщений типа Filters.text
@@ -49,6 +51,31 @@ def main():
 # Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
 def time(update, context):
     update.message.reply_text(now.strftime('%H:%M:%S'))
+
+
+def equations(update, context):
+    update.message.reply_text("Введите коэффициенты для уравнения")
+    update.message.reply_text("ax^2 + bx + c = 0:")
+    update.message.reply_text("a = ")
+    a = update.message.text
+    update.message.reply_text("b = ")
+    b = update.message.text
+    update.message.reply_text("c = ")
+    c = update.message.text
+
+    discr = b ** 2 - 4 * a * c
+    update.message.reply_text(print("Дискриминант D = %.2f" % discr))
+
+
+    if discr > 0:
+        x1 = (-b + math.sqrt(discr)) / (2 * a)
+        x2 = (-b - math.sqrt(discr)) / (2 * a)
+        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+    elif discr == 0:
+        x = -b / (2 * a)
+        update.message.reply_text("x = %.2f" % x)
+    else:
+        update.message.reply_text("Корней нет")
 
 
 def date(update, context):
